4cb709073bfe17e41ddfbb754f8203b21e6d1529
Jeison Cordoba <jeisonsmit@gmail.com>
2025-08-29 19:12:27 -0500

    Creation of ToString, Validate and console

--- Time/POO.Workshop01.Core/Time.cs
+++ Time/POO.Workshop01.Core/Time.cs
@@ -42,9 +42,101 @@ public class Time
         Minute = _minute;
         Second = _second;
     }
+
     // 3. Metodos // return -> que me devuelva o que me sobreescriba por medio de ToString
+    public int Hour
+    {
+        get => _hour;
+        set
+        { 
+            _hour = ValidateHour(value); 
+        }
+    }
+    public int Millisecond
+    {
+        get => _millisecond;
+        set
+        {
+            _millisecond = ValidateMillisecond(value);
+        }
+    }
+    public int Minute
+    {
+        get => _minute;
+        set
+        {
+            _minute = ValidateMinute(value);
+        }
+    }
+    public int Second
+    {
+        get => _second;
+        set
+        {
+            _second = ValidateSecond(value);
+        }
+    }
     public override string ToString()
     {
-        return $"{_hour}/{_minute}/{_second}/{_millisecond}";
+        return $"{_hour : 00}/{_minute: 00}/{_second: 00}/{_millisecond: 00}";
+    }
+
+    // Creacion de To String
+    public int ToHour()
+    {
+        return _hour * 3600000 + _minute * 60000 + _second * 1000 + _millisecond; 
+    }
+    public int ToMillisecond()
+    {
+        return _hour * 3600000 + _minute * 60000 + _second * 1000 + _millisecond;
+    }
+    public int ToMinute()
+    {
+        return _hour * 3600000 + _minute * 60000 + _second * 1000 + _millisecond;
+    }
+    public int ToSecond()
+    {
+        return _hour * 3600000 + _minute * 60000 + _second * 1000 + _millisecond;
+    }
+
+    // Los metodos privados van despues de los metodos public o publicos
+    private bool IsOtherday(Time t4)
+    {
+        // Un día son 86,400,000 milisegundos
+        return (this.ToMillisecond() + t4.ToMillisecond()) >= 86400000;
+    }
+    private int ValidateHour(int hour)
+    {
+        if (hour < 0)
+        {
+            throw new Exception("Hour cannot be less than 0");
+        }
+        return hour;
+    }
+    private int ValidateMillisecond(int millisecond)
+    {
+        if (millisecond < 0 && millisecond > 999)
+        {
+            throw new Exception("Hour cannot be less than 0");
+        }
+        return millisecond;
+    }
+
+    private int ValidateMinute(int minute)
+    {
+        if (minute < 0 && minute > 59)
+        {
+            throw new Exception("Hour cannot be less than 0");
+        }
+        return minute;
+    }
+
+    private int ValidateSecond(int second)
+    {
+        if (second < 0 && second > 59)
+        {
+            throw new Exception("Hour cannot be less than 0");
+        }
+        return second;
     }
 }
---
+++ Time/Workshop01.POO.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using POO.Workshop01.Core;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var t1 = new Time();
+        var t2 = new Time(14);
+        var t3 = new Time(9, 34);
+        var t4 = new Time(19, 45, 56);
+        var t5 = new Time(23, 3, 45, 678);
+
+        var times = new List<Time> { t1, t2, t3, t4, t5 };
+
+
+        Console.WriteLine(t1.ToString());
+        Console.WriteLine(t2.ToString());
+        Console.WriteLine(t3.ToString());
+        Console.WriteLine(t4.ToString());
+        Console.WriteLine(t5.ToString());
+        foreach (Time time in times)
+        {
+            Console.WriteLine($"Time: {time}");
+            Console.WriteLine($"\tMilliseconds: {time.ToMilliseconds,15:N0}");
+            Console.WriteLine($"\tSeconds     : {time.ToSeconds,15:N0}");
+            Console.WriteLine($"\tMinutes     : {time.ToMinutes,15:N0}");
+            Console.WriteLine($"\tAdd         : {time.ToAdd(t3),15:N0}");
+            Console.WriteLine($"\tIs Other day: {time.IsOtherday(t4)}");
+            Console.WriteLine();
+        }
+    }
+}
